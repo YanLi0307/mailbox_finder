403db782df48c9ca310dbe715bded95dfe942c26
Kris Luminar <kris.luminar@gmail.com>
2013-01-29 20:35:47 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailboxFinder::Application.config.secret_token = 'b4b2f4a3002dfd02ec400110c6fa0e26779496b9aaaef5109ab8e8fae20260cc913c6406f2dce13c718370a45ebef04f12297fa7e28d300a9c2bc53df0d4a528'
