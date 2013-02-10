9715a37ab601ec1239906ad018b1e386f3c625e2
Sol W <capnsol@gmail.com>
2013-02-09 17:44:44 -0800

    implement Mailbox#create

--- app/controllers/mailboxes_controller.rb
+++ app/controllers/mailboxes_controller.rb
@@ -2,4 +2,23 @@ class MailboxesController < ApplicationController
   def index
     @mailboxes = Mailbox.all
   end
+
+  def new
+    @mailbox = Mailbox.new
+  end
+
+  def create
+    @mailbox = Mailbox.new(params[:mailbox])
+    if @mailbox.save
+      flash[:notice] = "Mailbox got made."
+      redirect_to @mailbox
+    else
+      flash[:alert] = "Mailbox ain't made!"
+      render :action => "new"
+    end
+  end
+
+  def show
+    @mailbox = Mailbox.find(params[:id])
+  end
 end
