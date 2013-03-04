e9875f232698759fcace1c83a6405cfeb2ecf19e
Kris Luminar <kris.luminar@gmail.com>
2013-03-03 17:28:14 -0800

    clean up model

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,12 +3,10 @@ class Location < ActiveRecord::Base
   belongs_to :mailbox
 
   def to_s
-    "#{address}, #{city}, #{state}, United States, #{zip}"
+    normalized_address
   end
 
-  # acts_as_gmappable
   def gmaps4rails_address
-    #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
     "#{self.address}, #{self.city}, United States, #{self.zip}"
   end
 
